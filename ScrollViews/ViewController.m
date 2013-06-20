c163ef7d5777c944d388f9514f6955f8c0759fb5
Ryan Poolos <ryanpoolos@Gmail.com>
2013-06-19 22:52:23 -0400

    Have more advanced pieces working, but still far far from perfect.

--- ScrollViews/ViewController.m
+++ ScrollViews/ViewController.m
@@ -8,6 +8,46 @@
 
 #import "ViewController.h"
 
+@interface ClipView : UIView
+
+@end
+
+@implementation ClipView
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    UIView *scrollView = self.subviews.lastObject;
+    
+    if ([self pointInside:point withEvent:event]) {
+        if ([scrollView hitTest:point withEvent:event]) {
+            return [scrollView hitTest:point withEvent:event];
+        }
+        
+        return scrollView;
+    }
+    
+    return nil;
+}
+
+@end
+
+//@interface ClipScrollView : UIScrollView
+//
+//@end
+//
+//@implementation ClipScrollView
+//
+//- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+//{
+//    if ([self pointInside:point withEvent:event]) {
+//        return self.subviews.lastObject;
+//    }
+//    
+//    return nil;
+//}
+//
+//@end
+
 @interface ViewController () <UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource>
 {
     UIScrollView *superScrollView;
@@ -33,13 +73,20 @@
     CGFloat headerHeight = 224.0;
     CGFloat padding = 8.0;
     
-    superScrollView = [[UIScrollView alloc] initWithFrame:UIEdgeInsetsInsetRect(self.view.bounds, UIEdgeInsetsMake(128.0, 0.0, 0.0, 0.0))];
-    [superScrollView setContentSize:CGSizeMake(CGRectGetWidth(superScrollView.frame), CGRectGetHeight(superScrollView.frame) + headerHeight)];
-    [superScrollView setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"128-189.jpg"]]];
+    ClipView *hackView = [[ClipView alloc] initWithFrame:UIEdgeInsetsInsetRect(self.view.bounds, UIEdgeInsetsMake(128.0, 0.0, 0.0, 0.0))];
+    [hackView setBackgroundColor:[UIColor colorWithWhite:1.0 alpha:0.25]];
+    [hackView setClipsToBounds:YES];
+    [self.view addSubview:hackView];
+    
+    superScrollView = [[UIScrollView alloc] initWithFrame:hackView.bounds];//CGRectMake(0.0, 128.0, CGRectGetWidth(self.view.bounds), headerHeight)];//UIEdgeInsetsInsetRect(self.view.bounds, UIEdgeInsetsMake(128.0, 0.0, 0.0, 0.0))];
+    [superScrollView setContentSize:CGSizeMake(CGRectGetWidth(superScrollView.frame), CGRectGetHeight(hackView.frame) + headerHeight)];
+//    [superScrollView setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"128-189.jpg"]]];
     [superScrollView setAutoresizingMask:UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth];
+    [superScrollView setPagingEnabled:YES];
+//    [superScrollView setScrollEnabled:NO];
+    [superScrollView setClipsToBounds:NO];
     [superScrollView setDelegate:self];
-    [self.view addSubview:superScrollView];
-    [superScrollView setScrollEnabled:NO];
+    [hackView addSubview:superScrollView];
     
     topScrollView = [[UIScrollView alloc] initWithFrame:CGRectInset(CGRectMake(0.0, 0.0, CGRectGetWidth(superScrollView.frame), headerHeight), padding, padding)];
     [topScrollView setContentSize:CGSizeMake(CGRectGetWidth(topScrollView.frame) * 5.0, CGRectGetHeight(topScrollView.frame))];
@@ -48,7 +95,7 @@
     [topScrollView setDelegate:self];
     [superScrollView addSubview:topScrollView];
     
-    CGFloat leftRightScrollViewHeight = CGRectGetHeight(superScrollView.frame) - (padding * 2.0);
+    CGFloat leftRightScrollViewHeight = CGRectGetHeight(hackView.frame) - (padding * 2.0);
     
     leftScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(padding, headerHeight, 256.0 - (padding * 2.0), leftRightScrollViewHeight)];
     [leftScrollView setContentSize:CGSizeMake(CGRectGetWidth(leftScrollView.frame), CGRectGetHeight(leftScrollView.frame) * 5.0)];
@@ -63,6 +110,26 @@
     [rightScrollView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleBottomMargin];
     [rightScrollView setDelegate:self];
     [superScrollView addSubview:rightScrollView];
+}
+
+#pragma mark - Sub ScrollView Logic
+
+- (void)didBeginScrolling:(UIScrollView *)scrollView
+{
+    // Disable the SuperScrollView to ensure nothing funny happens
+    [superScrollView setScrollEnabled:NO];
+}
+
+- (void)scrollView:(UIScrollView *)scrollView didChangeOffset:(CGFloat)offset
+{
+    [superScrollView setContentOffset:CGPointMake(0, superScrollView.contentOffset.y + offset)];
+    
+    [scrollView setContentOffset:CGPointZero];
+}
+
+- (void)didEndScrolling:(UIScrollView *)scrollView
+{
+    [superScrollView setScrollEnabled:YES];
 }
 
 #pragma mark - UIScrollView Delegate
@@ -103,8 +170,69 @@
             [scrollView setContentOffset:CGPointZero];
         }
         
+        
+        
+//        // Did we just start dragging down
+//        if (offset > 0.0 && !scrollingSubView) {
+//            [self didBeginScrolling:scrollView];
+//            scrollingSubView = YES;
+//        }
+//        
+//        if (scrollingSubView) {
+//            CGFloat scrollOffset = offset;
+//            
+//            [self scrollView:scrollView didChangeOffset:scrollOffset];
+//        }
+
+//        if (offset <= 0.0) {
+//            // Stop scrolling when it reaches the top
+//            [scrollView setScrollEnabled:NO];
+//            
+//            CGFloat delta = MIN(0, offset);
+//            
+//            // Keep the scrolling going on the superScrollView
+//            // by adjust contentOffset by delta
+//            [superScrollView setContentOffset:CGPointMake(superScrollView.contentOffset.x, superScrollView.contentOffset.y - offset)];
+//        }
 
     }
 }
+
+//- (void)subScrollingEnded:(UIScrollView *)scrollView
+//{    
+//    // If the header is fully visible, we need to rest on it.
+//    if (CGRectContainsRect(superScrollView.bounds, topScrollView.frame)) {
+//        [superScrollView setContentOffset:CGPointZero];
+//    }
+//    
+//    // if the header is not fully visible, we need to decide
+//    // how visible it is and if we should hide or show it fully
+//    else {
+//        CGFloat headerLimit = CGRectGetMaxY(topScrollView.frame);
+//        CGFloat percentInvisible = superScrollView.contentOffset.y/headerLimit;
+//        
+//        if (percentInvisible > 0.51) {
+//            [superScrollView setContentOffset:CGPointMake(0, headerLimit)];
+//        } else {
+//            [superScrollView setContentOffset:CGPointZero];
+//        }
+//    }
+//}
+//
+//- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+//{
+//    if (!decelerate) {
+//        if (scrollView == leftScrollView || scrollView == rightScrollView) {
+//            [self subScrollingEnded:scrollView];
+//        }
+//    }
+//}
+//
+//- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+//{
+//    if (scrollView == leftScrollView || scrollView == rightScrollView) {
+//        [self subScrollingEnded:scrollView];
+//    }
+//}
 
 @end
